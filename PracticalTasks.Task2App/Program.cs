ec920aa64f5d3bfeacf28b96dfeaf04a40e194f5
Суворов Дмитрий (Suvorov_DmA) <Suvorov_DmA@directum.ru>
2024-10-24 16:59:00 +0500

    Ченовик задачи работы с рефлексией

--- PracticalTasks.Task2App/Program.cs
+++ PracticalTasks.Task2App/Program.cs
@@ -4,7 +4,26 @@
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      try
+      {
+        string assemblyName = "System.Windows.Forms";
+        string typeName = "System.Windows.Forms.MessageBox";
+
+        IEnumerable<string> nonStaticMethodsNames = ReflectionHelper.GetAllPublicNonStaticMethodsNames(assemblyName, typeName);
+        foreach (var name in nonStaticMethodsNames)
+        {
+          Console.WriteLine(name);
+        }
+
+        var showMethod = ReflectionHelper.GetPublicStaticOneParametricMethod(assemblyName, typeName, "Show");
+        showMethod?.Invoke(null, new object[] { "Привет, Медвед!" });
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex);
+      }
+
+      Console.ReadKey();
     }
   }
 }
---
+++ PracticalTasks.Task2App/ReflectionHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+
+namespace PracticalTasks.Task2App
+{
+  /// <summary>
+  /// Вспомогательный класс для работы с рефлексией.
+  /// </summary>
+  internal static class ReflectionHelper
+  {
+    #region Методы
+
+    /// <summary>
+    /// Получить имена всех общедоступных нестатических методов.
+    /// </summary>
+    /// <param name="assemblyName">Имя сборки.</param>
+    /// <param name="typeName">Имя типа.</param>
+    /// <returns>Перечисление имен методов.</returns>
+    /// <exception cref="ArgumentException">Если переданы пустые именя сборки и/или типа, то будет выбошно исключение.</exception>
+    /// <exception cref="Exception">Если тип не будет найден в сборке, то будет выброшено исключение.</exception>
+    public static IEnumerable<string> GetAllPublicNonStaticMethodsNames(string assemblyName, string typeName)
+    {
+      IEnumerable<MethodInfo> methodsInfos = GetMethodsInfos(assemblyName, typeName, BindingFlags.Public | BindingFlags.Instance);
+      var methodsNames = methodsInfos.Select(methodInfo => methodInfo.Name);
+      return methodsNames;
+    }
+
+    public static MethodInfo GetPublicStaticOneParametricMethod(string assemblyName, string typeName, string methodName)
+    {
+      IEnumerable<MethodInfo> methodsInfos = GetMethodsInfos(assemblyName, typeName, BindingFlags.Public | BindingFlags.Static);
+      return methodsInfos.First(methodInfo => methodInfo.Name == methodName && methodInfo.GetParameters().Count() == 1);
+    }
+
+    public static IEnumerable<MethodInfo> GetMethodsInfos(string assemblyName, string typeName, BindingFlags bindingFlags)
+    {
+      if (string.IsNullOrEmpty(assemblyName))
+      {
+        throw new ArgumentException($"'{nameof(assemblyName)}' cannot be null or empty.", nameof(assemblyName));
+      }
+
+      if (string.IsNullOrEmpty(typeName))
+      {
+        throw new ArgumentException($"'{nameof(typeName)}' cannot be null or empty.", nameof(typeName));
+      }
+
+      Assembly asm = Assembly.Load(assemblyName);
+      Type? type = asm.GetType(typeName, false, true) ?? throw new Exception($"Type {typeName} not found in assembly {assemblyName}");
+
+      return type.GetMethods(bindingFlags);
+    }
+
+    #endregion
+  }
+}

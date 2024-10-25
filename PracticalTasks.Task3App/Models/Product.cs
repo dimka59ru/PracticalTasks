35481469e9ba72f8b38df188c7cd76a842e11c3a
Суворов Дмитрий (Suvorov_DmA) <Suvorov_DmA@directum.ru>
2024-10-25 10:55:51 +0500

    Заготовка для третьего задания (с экселем)

---
+++ PracticalTasks.Task3App/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿namespace PracticalTasks.Task3App.Models
+{
+  /// <summary>
+  /// Продукт.
+  /// </summary>
+  internal class Product
+  {
+    #region Поля и свойства
+    /// <summary>
+    /// Наименование.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Цена.
+    /// </summary>
+    public decimal Price { get; }
+
+    #endregion
+
+    #region Констркторы
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="name">Наименование продукта.</param>
+    /// <param name="price">Цена продукта.</param>
+    /// <exception cref="ArgumentException">Если имя не задано или цена 0 или отрицательная, то будет выброшено исключение.</exception>
+    public Product(string name, decimal price)
+    {
+      if (string.IsNullOrEmpty(name))
+      {
+        throw new ArgumentException($"'{nameof(name)}' cannot be null or empty.", nameof(name));
+      }
+
+      if (price <= 0)
+      {
+        throw new ArgumentException($"'{nameof(price)}' must be greater than zero.", nameof(price));
+      }
+
+      this.Name = name;
+      this.Price = price;
+    }
+
+    #endregion
+  }
+}
---
+++ PracticalTasks.Task3App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticalTasks.Task3App
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
